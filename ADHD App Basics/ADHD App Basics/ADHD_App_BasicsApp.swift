b6bf4d5e6e95f42fa51e3d0ce899a89a6a44d0c9
Sahar <snbabaee@gmail.com>
2025-02-14 21:28:44 -0800

    project files created

---
+++ ADHD App Basics/ADHD App Basics/ADHD_App_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHD_App_BasicsApp.swift
+//  ADHD App Basics
+//
+//  Created by Sahar Babaei on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct ADHD_App_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADHD App Basics/ADHD App Basics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADHD App Basics
+//
+//  Created by Sahar Babaei on 2025-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
