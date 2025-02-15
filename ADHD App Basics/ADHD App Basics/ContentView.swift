0ad5bc1e286b20becf2d0728f21b2fbf51284895
Sahar <snbabaee@gmail.com>
2025-02-15 11:22:03 -0800

    couldn't style TabViews so created my own views using a selectedTab variable and buttons, used switch-case for content of each page

--- ADHD App Basics/ADHD App Basics/ContentView.swift
+++ ADHD App Basics/ADHD App Basics/ContentView.swift
@@ -8,76 +8,190 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = 1
+    // variable for changing tabs
+    
+    
     var body: some View {
+        
+        //MARK: - App's header title?
         HStack{
-            
             Text("Todo list app (rough draft)")
                 .font(.custom("Avenir-Medium", size: 17))
             //fun fact: this is how you change fonts.
             //doing fonts individually like this is kinda hell :/
-                
+            
             // TODO: mentor? how do i define font styles so i can apply them all over the project? (all heading 1s should be avenir bold 25, all body should be Helverica light 16, etc)
             
             Spacer()
-            //TODO: ^ not sure how this works but i had to put it in the middle of the two elements.
+            //^ not sure how this works but i had to put it in the middle of the two elements.
             Image(systemName: "checkmark.square")
                 .foregroundColor(.red)
-                           .font(.title)
+                .font(.title)
             
         }
         .padding(EdgeInsets(top: 0, leading: 10, bottom:10, trailing: 10))
         .background(Color.yellow)
-        //TODO: why doesn't this go full width?
-
+        //Note: why doesn't this go full width? - it did when i used a Spacer()
         
-        TabView{
-            //tab1:
-            Text("Tab 1 page")
-                .tabItem {
-                    Text("Tab 1")
-                    Image(systemName: "list.bullet.clipboard")
-                }
+        Spacer() // NOTE: without this, everything would appear at middle of screen.
+        
+        //MARK: you can't style tabs when using TabViews and TabItem(s), so commenting this out:
+        
+        //        TabView{
+        //            //tab1:
+        //            Text("Tab 1 page")
+        //                .tabItem {
+        //                    Text("Tab 1")
+        //                        .font(.system(size: 30))
+        //                    // why doesn't this change the size of the text?
+        //
+        //                    Image(systemName: "list.bullet.clipboard")
+        //                        .resizable()
+        //                        //.font(.system(size: 30)) //this didn't work
+        //                        .frame(width: 30, height: 30)
+        //                }
+        //
+        //            //tab2:
+        //            Text("Tab 2 page")
+        //                .tabItem {
+        //                    Text("Tab 2")
+        //                    Image(systemName: "plus")
+        //                }
+        //
+        //            //tab3:
+        //            Text("Tab 3 page")
+        //                .tabItem {
+        //                    Text("Tab 3")
+        //                    Image(systemName: "person.text.rectangle")
+        //                }
+        //
+        //            //tab4:
+        //            Text("Tab 4 page")
+        //                .tabItem {
+        //                    Text("Tab 4")
+        //                    Image(systemName: "face.smiling")
+        //                }
+        //
+        //            //tab5:
+        //            Text("Tab 5 page")
+        //                .tabItem {
+        //                    Text("Tab 5")
+        //                    Image(systemName: "eyes")
+        //                }
+        //
+        //
+        //            // fun fact: if you have more than 5, it'll move the rests to a "more tab"!
+        //            //tab6:
+        //            Text("Tab 6 page")
+        //                .tabItem {
+        //                    Text("Tab 6")
+        //                    Image(systemName: "checkmark.square")
+        //                }
+        //        }
+        
+        
+        
+        //MARK: - Content for each tab's page
+        
+        VStack{ //TODO: Mentor: feels weird to put a switch block in a VStack...
             
-            //tab2:
-            Text("Tab 2 page")
-                .tabItem {
-                    Text("Tab 2")
-                    Image(systemName: "plus")
-                }
-            
-            //tab3:
-            Text("Tab 3 page")
-                .tabItem {
-                    Text("Tab 3")
-                    Image(systemName: "person.text.rectangle")
-                }
-            
-            //tab4:
-            Text("Tab 4 page")
-                .tabItem {
-                    Text("Tab 4")
-                    Image(systemName: "face.smiling")
-                }
-            
-            //tab5:
-            Text("Tab 5 page")
-                .tabItem {
-                    Text("Tab 5")
-                    Image(systemName: "eyes")
-                }
-            
-            
-            // fun fact: if you have more than 5, it'll move the rests to a "more tab"!
-            //tab6:
-            Text("Tab 6 page")
-                .tabItem {
-                    Text("Tab 6")
-                    Image(systemName: "checkmark.square")
-                }
+            switch selectedTab{
+            case 1:
+                Text ("this is tab 1")
+            case 2:
+                Text ("this is tab 2")
+            case 3:
+                Text ("this is tab 3")
+            case 4:
+                Text ("this is tab 4")
+            case 5:
+                Text ("this is tab 5")
+            default:
+                Text ("this tab doesn't exist")
+                //TODO: Mentor? is using switch a good idea? the default case seems pointless...
+            }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //TODO: somehow this verically centers the text but i don't know why it doesn't automatically happen.
         
-
-        .padding()
+        
+        
+        //MARK: - the tabs
+        HStack{
+            
+            //tab 1
+            Button (action: {selectedTab = 1}){
+                VStack{
+                    Image(systemName: "list.bullet.clipboard")
+                        .font(.system(size: 25))
+                    //^ in SwiftUI, SF Symbols (system images) are treated like text glyphs, which is why their size is controlled using .font().
+                        .padding(.bottom,2)
+                    
+                    Text("tab 1")
+                        .font(.custom("Charter-Bold", size: 14))
+                }
+            }
+            
+            //tab 2
+            Button (action: {selectedTab = 2}){
+                VStack{
+                    Image(systemName: "person.text.rectangle")
+                        .font(.system(size: 25))
+                        .padding(.bottom,2)
+                    
+                    Text("tab 2")
+                        .font(.custom("Charter-Bold", size: 14))
+                    // TODO: Mentor? Do i have to set the font for each item every time? :c
+                }
+            }
+            
+            //tab 3
+            Button (action: {selectedTab = 3}){
+                VStack{
+                    Image(systemName: "plus")
+                        .font(.system(size: 25))
+                        .padding(.bottom,2)
+                    
+                    Text("tab 3")
+                        .font(.custom("Charter-Bold", size: 14))
+                }
+            }
+            
+            //tab 4
+            Button (action: {selectedTab = 4}){
+                VStack{
+                    Image(systemName: "face.smiling")
+                        .font(.system(size: 25))
+                        .padding(.bottom,2)
+                    
+                    Text("tab 4")
+                        .font(.custom("Charter-Bold", size: 14))
+                }
+            }
+            
+            //tab 5
+            Button (action: {selectedTab = 5}){
+                VStack{
+                    Image(systemName: "checkmark.square")
+                        .font(.system(size: 25))
+                        .padding(.bottom,2)
+                    
+                    Text("tab 5")
+                        .font(.custom("Charter-Bold", size: 14))
+                }
+            }
+        }
+        .frame(height: 60)
+        .frame(maxWidth: .infinity) //this fixes the width but not spacing between tabs
+        .clipped()
+        .background(Color.yellow)
+        
+        
+        
+        
+        
     }
 }
 
