3c9d3d9cd09228a7bb5e0af52c19a169303c7450
Sahar <snbabaee@gmail.com>
2025-02-17 22:44:44 -0800

    revised TaksCard's variables

--- ADHD App Basics/ADHD App Basics/views/TaskCard.swift
+++ ADHD App Basics/ADHD App Basics/views/TaskCard.swift
@@ -11,15 +11,24 @@ struct TaskCard: View {
     
 //MARK: - Variables
     //the initialized values are the default states for now
-    var taskName: String = ""
-    var taskStatus: String = "Not Started"
-    var taskTag: String = ""
-    var taskReminder: Bool = false
-    var taskReminderTime: TimeInterval = 0   // <---- not sure what datatype to use
-    var taskSubtask: Bool = false
-    var taskPriority: Int = 0
-    var taskDate: Date = Date() // <---- not sure how this would work
-    //TODO: ^^ Not sure what this does but it was suggested by xcode's auto complete
+    
+    var taskName: String = ""                //   <-- Task name
+    var taskStatus: String = "Not Started"   //   <-- Completion status
+    var taskTag = [String]()                 //   <-- Tags associated with it
+    var taskPriority: Int = 0                //   <-- what is the priority (0 is no priority)
+    var taskDuration: Int = 0                //   <-- how long the task would take
+    
+    var taskDueDate: Date = Date()           //   <-- the due date for the task
+    var taskReminder: Bool = false           //   <-- reminder to notify you to do the task
+    var taskReminderTime: TimeInterval = 0   //   <-- what time the reminder would go off
+    //TODO: ^not sure if i'm approaching this right
+    
+    var taskSubtask: Bool = false            //   <-- will the task have subtasks?
+    //TODO: mentor? ^how do i create variables for subtasks when i don't know how many subtasks there will be?
+    
+    var taskCreationDate: Date = Date()      //   <-- time/date the task was created
+    
+    //idea: add a variable to keep track of "overdue" tasks?
     
     
 //MARK: - body
