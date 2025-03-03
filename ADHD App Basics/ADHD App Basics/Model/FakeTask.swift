462f907824a7a2278cffe244a9bd30a19be54b0a
Sahar <snbabaee@gmail.com>
2025-03-03 14:12:00 -0800

    adding variables to FakeTask

--- ADHD App Basics/ADHD App Basics/Model/FakeTask.swift
+++ ADHD App Basics/ADHD App Basics/Model/FakeTask.swift
@@ -5,3 +5,18 @@
 //  Created by Sahar Babaei on 2025-03-03.
 //
 
+import Foundation
+import SwiftUI
+
+
+struct FakeTask{
+    var fTaskName: String = "this would be the task name"
+    
+    var fTaskID = UUID()
+    var fTaskCategory : [String] = [] //todo: Mentor? how do i give it a default value of "no tag" that can later change to the user's selected category?
+    var fTaskCompleted: Bool = false
+    var fTaskStatus: String = "Not Started"
+    var fTaskCreationDate : Date = Date()
+    var fTaskCompletionDate : Date = Date () //Todo: What should the default value for this be?
+    
+}
--- ADHD App Basics/ADHD App Basics/views/FakeTaskCard.swift
+++ ADHD App Basics/ADHD App Basics/views/FakeTaskCard.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct FakeTaskCard: View {
+    
+    var fTask : FakeTask
+ 
+    
+    //MARK: - body
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
     }
 }
 
+
+
+//MARK: - Preview
 #Preview {
-    FakeTaskCard()
+    FakeTaskCard(fTask: FakeTask())
 }
